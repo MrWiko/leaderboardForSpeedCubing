dd0c4481bc4be75b9107aad96f217b788164c7ff
Wiktor <wiktrox097@gmail.com>
2024-03-18 20:32:14 +0100

    jeszcze tylko sortowanie

--- index.js
+++ index.js
@@ -3,34 +3,57 @@ function generateTabel() {
     var contestants = [
         {
             name: "Michal Gmaj",
-            czas1: new Date(2018, 11, 24, 10, 33, 30, 0)
+            czas1: [new Date(2018, 11, 24, 10, 2, 30, 0), 0,0]
         },
         {
             name: "Marcin Najman",
-            czas1: new Date(2018, 11, 24, 11, 33, 30, 0)
+            czas1: [new Date(2018, 11, 24, 11, 3, 30, 0), 0,0]
         },
         {
             name: "Marcin Nienajman",
-            czas1: new Date(2018, 11, 24, 20, 33, 30, 0)
+            czas1: [new Date(2018, 11, 24, 20, 4, 30, 0), 0,0]
         },
         {
             name: "Adam Smasher",
-            czas1: new Date(2018, 11, 24, 12, 33, 30, 0)
+            czas1: [new Date(2018, 11, 24, 12, 5, 30, 0), 0, 0]
         }
     ]
 
-    for (const object of contestants){
-        for(var i = 0; i < 5; i++){
-            const tempCreator = document.createElement('div');
-            tempCreator.classList.add('grid-item'); 
-            if(i == 0)
-                tempCreator.innerHTML = object.name.split(' ')[0];
-            else if(i == 1)
-                tempCreator.innerHTML = object.name.split(' ')[1];
-            else if(i == 2)
-                tempCreator.innerHTML = object.czas1.getHours() + ":" + object.czas1.getMinutes();
+    var timeSum = new Date(0, 0, 0, 0, 0, 0, 0);
 
-            tabelaDisplay.appendChild(tempCreator);
+    for (const object of contestants){
+        timeSum = 0;
+        for(var i = 0; i < 4; i++){
+            if(i == 0){
+                const tempCreator = document.createElement('div');
+                tempCreator.classList.add('grid-item'); 
+                tempCreator.innerHTML = object.name.split(' ')[0];
+                tabelaDisplay.appendChild(tempCreator);
+            }
+            else if(i == 1){
+                const tempCreator = document.createElement('div');
+                tempCreator.classList.add('grid-item'); 
+                tempCreator.innerHTML = object.name.split(' ')[1];
+                tabelaDisplay.appendChild(tempCreator);
+            }
+            else if(i == 2){
+                for(times of object.czas1){
+                    const tempCreator = document.createElement('div');
+                    tempCreator.classList.add('grid-item'); 
+                    if(times != 0){
+                        tempCreator.innerHTML = times.getMinutes(); 
+                        timeSum += Date.parse(times);
+                    }
+                    else    
+                        tempCreator.innerHTML = '-'; 
+                    tabelaDisplay.appendChild(tempCreator);
+                }
+                const tempCreator = document.createElement('div');
+                tempCreator.classList.add('grid-item'); 
+                tempCreator.innerHTML = new Date(timeSum).getMinutes(); 
+                tabelaDisplay.appendChild(tempCreator);
+            }
+
         }
     }
 }
